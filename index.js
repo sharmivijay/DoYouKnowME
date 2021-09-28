c3e544c4aa86d7a1ba10987a116bc47c1a3ca7f3
SharmilaGnanasa <>
2021-09-28 02:54:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var questionList =[
+  {
+   question: "What is my favourite food? ",
+   answer:"Briyani"
+  },
+  {
+    question:"What is my favourite sport? ",
+    answer:"volley ball"
+  },
+  {
+    question:"What is my favourite colour? ",
+    answer:"Green"
+  },
+  {
+    question:"where do i live? ",
+    answer:"Trichy"
+  },
+  {
+    question:"what is my favourite pet? ",
+    answer:"dog"
+  }
+]
+
+
+
+function checkAnswer(question, answer)
+{
+  var userResponse = readLineSync.question(question);
+  if(userResponse.toUpperCase() === answer.toUpperCase()){
+    console.log("Cool ... you are right!");
+  }
+  else{
+    console.log("No..better luck next time..");
+  }
+}
+
+var readLineSync = require("readline-sync");
+//var readLineSync = require("readline-sync");
+var username = readLineSync.question("May i know your name : ");
+
+console.log("Welcome "+ username + " to the game of How well do you know me ");
+for(var i=0; i<questionList.length; i++){
+  checkAnswer(questionList[i].question, questionList[i].answer);
+}
+
