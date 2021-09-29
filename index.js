0ec7d5fb16865ecba9fb350f5198f68bd90e99d8
sharmivijay <89096043+sharmivijay@users.noreply.github.com>
2021-09-29 16:46:45 +0000

    feat - add score based on the ans

--- index.js
+++ index.js
@@ -27,18 +27,26 @@ function checkAnswer(question, answer)
   var userResponse = readLineSync.question(question);
   if(userResponse.toUpperCase() === answer.toUpperCase()){
     console.log("Cool ... you are right!");
+    return true;
   }
   else{
     console.log("No..better luck next time..");
+    return false;
   }
 }
 
 var readLineSync = require("readline-sync");
 //var readLineSync = require("readline-sync");
 var username = readLineSync.question("May i know your name : ");
+var score = 0;
 
 console.log("Welcome "+ username + " to the game of How well do you know me ");
 for(var i=0; i<questionList.length; i++){
-  checkAnswer(questionList[i].question, questionList[i].answer);
+  var response= checkAnswer(questionList[i].question, questionList[i].answer);
+  if(response){
+    score = score+1;
+  }
+  
 }
 
+console.log("Hey your score: ",score);
